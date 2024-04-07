a323ba9eaea46ea6c698873c92546e0868788063
Nikita Pyshchev <npyshchev@igatec.com>
2024-04-07 03:27:53 +0300

    add oop tasks

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,5 +1,34 @@
 package com.epam.OOP;
 
 public class Animal {
+    private final String color;
+    private final int numberOfPaws;
+    private final boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        return String.format(
+                "This animal is mostly %s. It has %s paws and %s fur.",
+                this.color,
+                this.numberOfPaws,
+                this.hasFur ? "a" : "no"
+        );
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
 }
--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -1,5 +1,15 @@
 package com.epam.OOP;
 
-public class Bird {
+public class Bird extends Animal {
 
+    public Bird() {
+        super("blue", 2, false);
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format("%s %s", super.getDescription(),
+                String.format("Moreover, it has %s wings and can fly.", getNumberOfPaws())
+        );
+    }
 }
