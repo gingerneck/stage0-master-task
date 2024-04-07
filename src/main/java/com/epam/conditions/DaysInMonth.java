6f54d007ab99756ab8abfaddbf68d02e5be4836e
Nikita Pyshchev <npyshchev@igatec.com>
2024-04-07 17:52:42 +0300

    done with task conditions

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -1,9 +1,17 @@
 package com.epam.conditions;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
-
+        if (year < 0
+                || month < 0 || month > 12) {
+            System.out.println("invalid date");
+        } else {
+            Calendar calendar = new GregorianCalendar(year, month - 1, 1);
+            System.out.println(calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+        }
     }
-
 }
