fcd25d0bc825183940379b71300a3901df876694
Nikita Pyshchev <npyshchev@igatec.com>
2024-04-24 22:51:36 +0300

    add pyramid tasks

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,6 +3,22 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
+        if (cathetusLength == 0) {
+            System.out.print("");
+        } else if (cathetusLength == 1) {
+            System.out.println("1");
+        } else if (cathetusLength > 1) {
+            for (int i = 1; i < cathetusLength + 1; i++) {
+
+                for (int j = 0; j < cathetusLength - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int k = i; k > 0; k--) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
 
     }
 
--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -4,12 +4,22 @@ public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
         if (cathetusLength == 0) {
-            System.out.println("");
+            System.out.print("");
         } else if (cathetusLength == 1) {
             System.out.println("1");
         } else if (cathetusLength > 1) {
-            for (int i = cathetusLength; i != 0; i--) {
+            for (int i = 1; i < cathetusLength + 1; i++) {
 
+                for (int j = 0; j < cathetusLength - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int k = i; k > 1; k--) {
+                    System.out.print(k);
+                }
+                for (int k = 1; k <= i; k++) {
+                    System.out.print(k);
+                }
+                System.out.println();
             }
         }
     }
